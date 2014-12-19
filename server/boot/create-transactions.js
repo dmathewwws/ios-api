6af4a9e25075b1467c11c9c679c57f0c52848f2f
dannylmathews <dannyl.mathews@gmail.com>
2014-12-19 12:14:01 -0800

    test git

--- server/boot/create-transactions.js
+++ server/boot/create-transactions.js
@@ -13,3 +13,18 @@ module.exports = function(app) {
     });
   });
 };
+
+
+module.exports = function(app) {
+  app.dataSources.mysqlDs.automigrate('User', function(err) {
+    if (err) throw err;
+
+    app.models.User.create([
+      {device_id: '123ABC', verfication_code: 'AAAA-BBBB-CCCC', number: '778-668-9726'}
+    ], function(err, userNames) {
+      if (err) throw err;
+
+      console.log('Users created: \n', userNames);
+    });
+  });
+};
